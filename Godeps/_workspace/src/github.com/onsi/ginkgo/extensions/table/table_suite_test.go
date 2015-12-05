02461b59ca7da280dc91861f5375f828c4f9d68c
teddyking <ed@teddyking.co.uk>
2015-12-05 12:24:28 +0000

    Add Godeps

---
+++ Godeps/_workspace/src/github.com/onsi/ginkgo/extensions/table/table_suite_test.go
@@ -0,0 +1,13 @@
+package table_test
+
+import (
+	. "github.com/teddyking/tidyblob/Godeps/_workspace/src/github.com/onsi/ginkgo"
+	. "github.com/teddyking/tidyblob/Godeps/_workspace/src/github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestTable(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Table Suite")
+}
